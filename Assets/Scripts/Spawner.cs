85aad29dfd4909aae0a29748780027845319b204
Jeff Goodhue <a>
2019-01-26 11:59:36 -0800

    Updated the spawner to allow multiple objects with different rates. Allows objects to spawn in "Bursts"

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
@@ -13,8 +12,8 @@ public class Spawner : MonoBehaviour
     [SerializeField]
     private float stepDistance;
 
-    [SerializeField, Range(0, 100)]
-    private float stepChance;
+    [SerializeField]
+    private SpawnerElement[] spawnElements;
 
     private void Start()
     {
@@ -33,23 +32,54 @@ public class Spawner : MonoBehaviour
                 // Move towards node at the given "StepDistance"
                 currentPosition = Vector3.MoveTowards(currentPosition, node.position, stepDistance);
 
-                // Roll to see if we should spawn a cell
-                if (Random.Range(0, 100) < stepChance)
+                foreach (SpawnerElement spawnerElement in spawnElements)
                 {
-                    // Get the forward vector's rotation
-                    Quaternion rotation = Quaternion.LookRotation(direction);
+                    // Roll to see if we should spawn a cell
+                    if (UnityEngine.Random.Range(0, 100) < spawnerElement.spawnChance)
+                    {
+                        // Get the forward vector's rotation
+                        Quaternion rotation = Quaternion.LookRotation(direction);
 
-                    // Roll a random angle and return the rotation
-                    Quaternion randomAngle = Quaternion.AngleAxis(Random.Range(0f, 360f), Vector3.forward);
+                        // Roll a random angle and return the rotation
+                        Quaternion randomAngle = Quaternion.AngleAxis(UnityEngine.Random.Range(0f, 360f), Vector3.forward);
 
-                    // Roll a random radius (distance from the rail)
-                    Vector3 randomRadius = new Vector3(0f, Random.Range(0f, offsetMax));
+                        // Roll a random radius (distance from the rail)
+                        Vector3 randomRadius = new Vector3(0f, UnityEngine.Random.Range(0f, offsetMax));
 
-                    // apply the random angle to the random radius, then apply the forward vector.
-                    Vector3 offset = rotation * (randomAngle * randomRadius);
-                    Instantiate(cellPrefab, currentPosition + offset, Quaternion.identity);
+                        // apply the random angle to the random radius, then apply the forward vector.
+                        Vector3 offset = currentPosition + (rotation * (randomAngle * randomRadius));
+                        if (!spawnerElement.burst)
+                        {                            
+                            Instantiate(spawnerElement.prefab, offset, Quaternion.identity);
+                        }
+                        else
+                        {
+                            int spawnCount = (int)UnityEngine.Random.Range(0.80f * spawnerElement.burstFactor, 
+                                1.20f * spawnerElement.burstFactor);
+                            for (int i = 0; i < spawnCount; i++)
+                            {
+                                Vector3 burstOffset = offset + (UnityEngine.Random.rotationUniform *
+                                    (Vector3.up * UnityEngine.Random.Range(0, spawnerElement.burstDistance)));
+
+                                GameObject obj = Instantiate(spawnerElement.prefab, burstOffset, Quaternion.identity);
+                                obj.GetComponent<Renderer>().material.color = Color.red;
+                            }
+                        }
+                    }
                 }
             }
         }
+    }
+
+    [Serializable]
+    private struct SpawnerElement
+    {
+        public GameObject prefab;
+
+        [Range(0, 100)]
+        public float spawnChance;
+        public bool burst;
+        public int burstFactor;
+        public float burstDistance;
     }
 }
