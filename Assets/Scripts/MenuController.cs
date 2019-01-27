5a59df0b9e6558ef01b6c0fe044fa60af7b72f2c
ftrbreed <dan@future-breed.com>
2019-01-26 17:31:50 -0800

    Some really basic main menu stuff, press A/space to start

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum Scenes : int
+{
+    MainMenu = 0,
+    OsmosisScene
+}
+
+public class MenuController : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        bool aButtonPressed = Input.GetButtonUp("Fire1");
+        if (aButtonPressed)
+        {
+            SceneManager.LoadScene((int)Scenes.OsmosisScene);
+        }
+    }
+}
