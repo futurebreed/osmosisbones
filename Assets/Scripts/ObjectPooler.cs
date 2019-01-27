e9dc5ef5e0277b06da636a81a12f0894c29636ae
Jeff Goodhue <a>
2019-01-26 19:22:19 -0800

    Spawner now grabs from an object pooler. Currently does not release objects back to the pool. Updated White blood cell model

---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefab;
+
+    [SerializeField]
+    private int startCount;
+
+    private Queue<GameObject> pool;
+
+    private void Start()
+    {
+        pool = new Queue<GameObject>();
+        for (int i = 0; i < startCount; i++)
+        {
+            GameObject obj = Instantiate(prefab);
+            obj.SetActive(false);
+            pool.Enqueue(obj);
+        }
+    }
+
+    public GameObject GetObject(Vector3 position, Quaternion rotation)
+    {
+        GameObject obj;
+        if (pool.Count != 0)
+        {
+            obj = pool.Dequeue();
+            obj.transform.position = position;
+            obj.transform.rotation = rotation;
+            obj.SetActive(true);
+        }
+        else
+        {
+            obj = Instantiate(prefab, position, rotation);
+        }
+        return obj;
+    }
+
+    public void ReleaseObject(GameObject obj)
+    {
+        obj.SetActive(false);
+        pool.Enqueue(obj);
+    }
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -15,70 +15,87 @@ public class Spawner : MonoBehaviour
     [SerializeField]
     private SpawnerElement[] spawnElements;
 
+    private int i = 0;
+    private Vector3 currentPosition;
+
     private void Start()
     {
         // Make sure cells don't spawn right next to the start position
         // What determines start position? Currently just using Vector3.zero
-        Vector3 currentPosition = Vector3.MoveTowards(Vector3.zero, railManager.GetNodes()[0].position, 5f);
-        float offsetMax = railManager.GetOffsetMax();
+        currentPosition = Vector3.MoveTowards(Vector3.zero, railManager.GetNodes()[0].position, 5f);
         // Traverse through the nodes
-        foreach (RailManager.Node node in railManager.GetNodes())
+        while (i < 3)
         {
-            if (node.cinematic)
-            {
-                currentPosition = node.position;
-                continue;
-            }
-            // Loop until we are close enough to move on to the next node
-            while ((node.position - currentPosition).sqrMagnitude != 0f)
-            {
-                // Get the direction towards the node before we move towards it
-                Vector3 direction = node.position - currentPosition;
-                // Move towards node at the given "StepDistance"
-                currentPosition = Vector3.MoveTowards(currentPosition, node.position, stepDistance);
+            InstantiateNextNode();
+        }
+    }
 
-                foreach (SpawnerElement spawnerElement in spawnElements)
+    public void InstantiateNextNode()
+    {
+        if (i >= railManager.GetNodes().Length)
+        {
+            return;
+        }
+        RailManager.Node node = railManager.GetNodes()[i];
+        if (node.cinematic)
+        {
+            currentPosition = node.position;
+            i++;
+            return;
+        }
+        float offsetMax = railManager.GetOffsetMax();
+        // Loop until we are close enough to move on to the next node
+        while ((node.position - currentPosition).sqrMagnitude != 0f)
+        {
+            // Get the direction towards the node before we move towards it
+            Vector3 direction = node.position - currentPosition;
+            // Move towards node at the given "StepDistance"
+            currentPosition = Vector3.MoveTowards(currentPosition, node.position, stepDistance);
+
+            foreach (SpawnerElement spawnerElement in spawnElements)
+            {
+                // Roll to see if we should spawn a cell
+                if (UnityEngine.Random.Range(0, 100) < spawnerElement.spawnChance)
                 {
-                    // Roll to see if we should spawn a cell
-                    if (UnityEngine.Random.Range(0, 100) < spawnerElement.spawnChance)
+                    // Get the forward vector's rotation
+                    Quaternion rotation = Quaternion.LookRotation(direction);
+
+                    // Roll a random angle and return the rotation
+                    Quaternion randomAngle = Quaternion.AngleAxis(UnityEngine.Random.Range(0f, 360f), Vector3.forward);
+
+                    // Roll a random radius (distance from the rail)
+                    Vector3 randomRadius = new Vector3(0f, UnityEngine.Random.Range(0f, offsetMax));
+
+                    // apply the random angle to the random radius, then apply the forward vector.
+                    Vector3 offset = currentPosition + (rotation * (randomAngle * randomRadius));
+                    if (!spawnerElement.burst)
                     {
-                        // Get the forward vector's rotation
-                        Quaternion rotation = Quaternion.LookRotation(direction);
-
-                        // Roll a random angle and return the rotation
-                        Quaternion randomAngle = Quaternion.AngleAxis(UnityEngine.Random.Range(0f, 360f), Vector3.forward);
-
-                        // Roll a random radius (distance from the rail)
-                        Vector3 randomRadius = new Vector3(0f, UnityEngine.Random.Range(0f, offsetMax));
-
-                        // apply the random angle to the random radius, then apply the forward vector.
-                        Vector3 offset = currentPosition + (rotation * (randomAngle * randomRadius));
-                        if (!spawnerElement.burst)
-                        {                            
-                            Instantiate(spawnerElement.prefab, offset, Quaternion.identity);
-                        }
-                        else
+                        spawnerElement.prefabPooler.GetObject(offset, Quaternion.identity);
+                        //Instantiate(spawnerElement.prefab, offset, Quaternion.identity);
+                    }
+                    else
+                    {
+                        int spawnCount = (int)UnityEngine.Random.Range(0.80f * spawnerElement.burstFactor,
+                            1.20f * spawnerElement.burstFactor);
+                        for (int i = 0; i < spawnCount; i++)
                         {
-                            int spawnCount = (int)UnityEngine.Random.Range(0.80f * spawnerElement.burstFactor, 
-                                1.20f * spawnerElement.burstFactor);
-                            for (int i = 0; i < spawnCount; i++)
-                            {
-                                Vector3 burstOffset = offset + (UnityEngine.Random.rotationUniform *
-                                    (Vector3.up * UnityEngine.Random.Range(0, spawnerElement.burstDistance)));
+                            Vector3 burstOffset = offset + (UnityEngine.Random.rotationUniform *
+                                (Vector3.up * UnityEngine.Random.Range(0, spawnerElement.burstDistance)));
 
-                                GameObject obj = Instantiate(spawnerElement.prefab, burstOffset, Quaternion.identity);
-                            }
+                            spawnerElement.prefabPooler.GetObject(burstOffset, Quaternion.identity);
+                            //GameObject obj = Instantiate(spawnerElement.prefab, burstOffset, Quaternion.identity);
                         }
                     }
                 }
             }
         }
+        i++;
     }
 
     [Serializable]
     private struct SpawnerElement
     {
-        public GameObject prefab;
+        public ObjectPooler prefabPooler;
 
         [Range(0, 100)]
         public float spawnChance;
